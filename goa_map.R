2ac51472580185220427a6537ceaa52307f06157
MattCallahan-NOAA <matt.callahan@noaa.gov>
2022-07-15 16:49:11 -0800

    clarified text

--- goa_map.R
+++ goa_map.R
@@ -12,7 +12,7 @@ r.ak2 <- marmap::as.raster( r.ak)*-1
 
 
 #convert to contour
-depth_c<-rasterToContour(r.ak2, levels=c(10,50,200))%>%
+depth_c<-rasterToContour(r.ak2, levels=c(200))%>%
   st_as_sf()
 
 ak<-AK_basemap()
@@ -22,9 +22,11 @@ nmfs<-AK_marine_area()%>% filter(NMFS_REP_AREA %in% c(610,620,630))
 png("goa_depth_map.png")
 ggplot()+
   geom_sf(data=nmfs, fill=NA)+
-  geom_sf(data=depth_c, aes(color=level))+
+  #geom_sf(data=depth_c, aes(color=level))+
+  geom_sf(data=depth_c, color="dark blue")+
   geom_sf(data=ak)+
   coord_sf(xlim=c(-175,-145),ylim=c(48,61))+
-  scale_color_manual(values=c("light blue",  "dark blue", "blue"))+
+  #scale_color_manual(values=c("light blue",  "dark blue", "blue"))+
+  ggtitle("NMFS 610-630 with 200m depth contour (blue)")+
   theme_bw()
 dev.off()
